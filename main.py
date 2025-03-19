a4c53721bb29c868ec367f9b4a8d425254622547
Sam McGuire <45981861+SamJMcguire@users.noreply.github.com>
2025-03-20 09:53:36 +1300

    Update main.py
    added title
    added functions for ...

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
 '''This is a place holder'''
+'''
+Rumplestilkson 
+author: Sam McGuire
+version: 1
+
+Description:  This is a guessing game based on the tale
+of Rumplestilskon.  Users have three chances to guess the 
+name randomly selected from the list
+
+'''
+
+# Import library or external code. OS for sleep and clear screen
+import os
+
+# Set variables for list, user guess, guess counter 
+
+INT_GUESSREMAINING = 3
+bool_endLoop = False
+str_userGuess = ''
+str_userName = ''
+
+# Creating game functions
+
+def greeting():
+    str_userName = input("Hello.  What is your name?")
+    print(f"Hello {str_userName}")
+    
+    
+# The Main program
+greeting()
